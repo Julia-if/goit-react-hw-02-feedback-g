043af8f729c593d08c4c15ee37efb5ea871b010f
Julia-if <juliaif18@gmail.com>
2022-07-26 13:01:24 +0300

    name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from "react";
+
+ export class App extends Component {
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
+onLeaveFeedback=(e)=>{
+
+const items = Object.keys(this.state)
+const nameBtn = items[0];
+console.log(nameBtn);
+}
+render(){
+ return (
+  <div>
+
+    <button type="button" data-name="Good" onClick={this.onLeaveFeedback}>Good</button>
+    <button type="button" data-name="Neutral" onClick={this.onLeaveFeedback}>Neutral</button>
+    <button type="button" data-name="Bad" onClick={this.onLeaveFeedback}s>Bad</button>
+  </div>
+ )
+}
+}
+
